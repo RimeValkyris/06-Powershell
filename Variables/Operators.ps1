ea5e195305d51074db7eea38c5843cd2f7d85310
YaranaikaKiryu <146524104+YaranaikaKiryu@users.noreply.github.com>
2025-11-06 15:22:52 +0800

    Add multiplication, division, and modulus operations to Operators.ps1

--- Variables/Operators.ps1
+++ Variables/Operators.ps1
@@ -14,3 +14,25 @@ $e = 7
 
 $difference = $d - $e
 Write-Host "Difference: $difference"
+
+Write-Host "-----------------------"
+# Multiplication
+$f = 4
+$g = 6
+
+$product = $f * $g
+Write-Host "Product: $product"
+
+Write-Host "-----------------------"
+# Division
+$h = 20
+$i = 4
+$quotient = $h / $i
+Write-Host "Quotient: $quotient"
+Write-Host "-----------------------"
+# Modulus
+$j = 22
+$k = 5
+$remainder = $j % $k
+Write-Host "Remainder: $remainder"
+Write-Host "-----------------------"
